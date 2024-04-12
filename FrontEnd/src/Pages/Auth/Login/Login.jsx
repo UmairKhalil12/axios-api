4d129823c95576465df547475301ec8df4026b3a
Saad Khalil <khalil.umair21@gmail.com>
2024-04-12 11:18:31 +0500

    'second'

--- FrontEnd/src/Pages/Auth/Login/Login.jsx
+++ FrontEnd/src/Pages/Auth/Login/Login.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
+import { useNavigate } from 'react-router-dom';
+import { GET_METHOD } from '../../../Backend/index';
+
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [confirmPass, setConfirmPass] = useState('');
+
+    const [data, setData] = useState({
+        email: '',
+        pass: ''
+    });
+
+    const navigate = useNavigate();
+
+    const handleLoginForm = (e) => {
+        e.preventDefault();
+        if (email !== '' && pass !== '' && confirmPass !== '') {
+            if (pass === confirmPass) {
+                setData({
+                    email: email,
+                    pass: pass
+                })
+                GET_METHOD(`?Email=${email}&Password=${pass}`)
+            }
+            else {
+                window.alert('Pass and confirm pass should be same');
+            }
+
+        }
+        else {
+            window.alert('All fields are required');
+        }
+    }
+
+
     return (
         <div className='login-form'>
             <h1>Login Form</h1>
-            <form className='form'>
-                <input placeholder='enter email'></input>
-                <input placeholder='enter password'></input>
-                <input placeholder='confirm password'></input>
+            <form className='form' onSubmit={handleLoginForm}>
+                <input
+                    placeholder='enter email'
+                    className='input-field'
+                    value={email}
+                    onChange={(e) => { setEmail(e.target.value) }} />
 
-                <button className='login-btn'>Login</button>
+                <input
+                    placeholder='enter password'
+                    className='input-field'
+                    value={pass}
+                    onChange={(e) => { setPass(e.target.value) }}
+                />
+
+                <input
+                    placeholder='confirm password'
+                    className='input-field'
+                    value={confirmPass}
+                    onChange={(e) => { setConfirmPass(e.target.value) }}
+                />
+
+                <button className='login-btn' >Login</button>
+                <p onClick={() => navigate('/signup')}>No account? want to create one ?</p>
             </form>
         </div>
     )
--- FrontEnd/src/Pages/Auth/Signup/Signup.jsx
+++ FrontEnd/src/Pages/Auth/Signup/Signup.jsx
@@ -1,22 +1,128 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Signup.css'
+import { useNavigate } from 'react-router-dom';
+import { GET_POST_METHOD } from '../../../Backend';
+
 
 export default function Signup() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [pass, setPass] = useState('');
+    const [address, setAddress] = useState('');
+    const [location, setLocation] = useState('');
+    const [showRoom, setShowRoom] = useState('');
+    const [identifcation, setIdentification] = useState('');
+
+    const navigate = useNavigate();
+
+    // const [data, setData] = useState({
+    //     name: '',
+    //     email: '',
+    //     phone: '',
+    //     pass: '',
+    //     address: '',
+    //     location: '',
+    //     showRoom: '',
+    //     identifcation: ''
+    // })
+
+    const handleSignForm = async (e) => {
+        e.preventDefault();
+        if (name !== '' && email !== '' && phone !== '' && pass !== ''
+            && address !== '' && location !== '' && showRoom !== '' && identifcation !== '') {
+
+            try {
+                await GET_POST_METHOD(
+                    `?Name=${name}&Email=${email}&PhoneNo=${phone}&Password=${pass}&Address=${address}&Location=${location}&ShowRoomName=${showRoom}&IdentificationNo=${identifcation}&ParentUserID=1&UserType=0`,
+                    {
+                        Name: name,
+                        Email: email,
+                        PhoneNo: phone,
+                        Password: pass,
+                        Address: address,
+                        Location: location,
+                        ShowRoomName: showRoom,
+                        IdentificationNo: identifcation,
+                        ParentUserID: 1,
+                        UserType: 0
+                    });
+                window.alert('User added successfully');
+            } catch (error) {
+                console.error('Error creating user:', error);
+                window.alert('Error creating user');
+            }
+        }
+        else {
+            window.alert('All fields are required');
+        }
+    }
+
+
     return (
         <div className='signup-form'>
             <h1>Signup Form</h1>
-            <form className='form'>
-                <input placeholder='enter name'></input>
-                <input placeholder='enter email'></input>
-                <input placeholder='enter phone number'></input>
-                <input placeholder='enter password'></input>
-                <input placeholder='confirm password'></input>
-                <input placeholder='enter address'></input>
-                <input placeholder='enter location'></input>
-                <input placeholder='enter show room name'></input>
-                <input placeholder='enter identifcation number'></input>
+            <form className='form' onSubmit={handleSignForm}>
+                <input
+                    placeholder='enter name'
+                    value={name}
+                    className='input-field'
+                    onChange={(e) => setName(e.target.value)}
+                />
 
-                <button className='signup-btn'  type='submit'>Signup</button>
+                <input
+                    placeholder='enter email'
+                    value={email}
+                    className='input-field'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <input
+
+                    placeholder='enter phone number'
+                    value={phone}
+                    className='input-field'
+                    onChange={(e) => setPhone(e.target.value)}
+                />
+
+                <input
+                    placeholder='enter password'
+                    value={pass}
+                    className='input-field'
+                    onChange={(e) => setPass(e.target.value)}
+                />
+
+                <input
+                    placeholder='enter address'
+                    value={address}
+                    className='input-field'
+                    onChange={(e) => setAddress(e.target.value)}
+                />
+
+                <input
+                    placeholder='enter location'
+                    value={location}
+                    className='input-field'
+                    onChange={(e) => setLocation(e.target.value)}
+
+                />
+
+                <input
+                    placeholder='enter show room name'
+                    value={showRoom}
+                    className='input-field'
+                    onChange={(e) => setShowRoom(e.target.value)}
+                />
+
+                <input
+                    placeholder='enter identifcation number'
+                    value={identifcation}
+                    className='input-field'
+                    onChange={(e) => setIdentification(e.target.value)}
+                />
+
+                <button className='signup-btn' type='submit'>Signup</button>
+                <p onClick={() => navigate('/login')}>already have an account? Login.</p>
             </form>
 
         </div>
