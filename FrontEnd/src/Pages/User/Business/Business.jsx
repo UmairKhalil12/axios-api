90dfed345d644a75ef21a7af173c344bdf52097d
Saad Khalil <khalil.umair21@gmail.com>
2024-06-10 08:07:12 +0500

    8th

--- FrontEnd/src/Pages/User/Business/Business.jsx
+++ FrontEnd/src/Pages/User/Business/Business.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react'
+import "./Business.css"
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { GET_BUSINESS_BY_ID } from '../../../Axios/axios';
+import Navbar from "../../../Components/Navbar/Navbar"
 
 export default function Business() {
     const id = useParams();
     const ID = id.id;
-    console.log("business id ",ID);
+    console.log("business id ", ID);
     const [business, setBusiness] = useState([]);
     const fetchData = async () => {
         try {
@@ -19,21 +21,34 @@ export default function Business() {
     }
     useEffect(() => {
         fetchData();
-    })
+    }, [id, business])
     return (
-        <div>
-            <h1>Business Data</h1>
-            {business?.map((value, index) => {
-                return (
-                    <div>
-                        {console.log('business by id', value)}
-                        <h3>{value?.BusinessName}</h3>
-                        <p>{index}</p>
-                        <p>{value?.BusinessOwnerName}</p>
-                    </div>
-                )
-            })}
+        <>
+            <Navbar />
+            <div className='business-container'>
+                <h1>Business Data</h1>
+                {business?.map((value, index) => {
+                    return (
+                        <div className='business-info-main'>
+                            {console.log('business by id', value)}
+                            <div className='cover-container'>
+                                <img src={value?.BusinessLogo} alt={value?.BusinessName} className='business-cover' />
+                            </div>
 
-        </div>
+                            <div>
+                                <h1>{value?.BusinessName}</h1>
+                                <div className='business-info'>
+                                    <p><span><b>Owner's Name : </b></span>{value?.BusinessOwnerName}</p>
+                                    <p><span><b>Website : </b></span>{value?.WebSite}</p>
+                                    <p><span><b>Business Number : </b></span>{value?.BusinessNumber}</p>
+                                    <p><span><b>Email : </b></span>{value?.Email}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+
+            </div>
+        </>
     )
 }
